b7c85215bf2af9a084c5a11400f2a459c92a2de4
vellampallikrishnasaigifhub <krishnasaivellampalli@gmail.com>
2024-09-19 16:04:58 +0530

    update

--- password input/pass.py
+++
@@ -1,8 +0,0 @@
-from getpass import getpass
-
-username = input('Enter the Username: ')
-password = getpass('Enter the password: ')
-
-print(f'Username: {username}')
-print(f'password: {password}')
-print('Password entered successfully.')
