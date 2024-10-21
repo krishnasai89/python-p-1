a773071145651aa6c4214f370fcd5211dcfc137b
vellampallikrishnasaigifhub <krishnasaivellampalli@gmail.com>
2024-10-21 11:32:45 +0530

    small changes in age calculator

--- Age Calculator/main.py
+++ Age Calculator/main.py
@@ -1,8 +1,12 @@
+from datetime import date
+
 a = input("Enter your name : ")
 
 b =  int(input("Enter your birth year : "))
 
-c= int(input("Enter the present  year : "))
+# c= int(input("Enter the present  year : "))
+
+c = date.today().year
 
 b -= c
 
